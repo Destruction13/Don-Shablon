be64acbbf7be7461732d28a2596c2b356d78a5d9
Destruction13 <deymanmaccenegy@gmail.com>
2025-06-07 11:59:33 +0300

    Fix debug image generation and parsing

--- tests/test_ocr_parse.py
+++ tests/test_ocr_parse.py
@@ -11,10 +11,50 @@ qt_gui.QGuiApplication = object
 sys.modules.setdefault('PySide6.QtGui', qt_gui)
 qt_widgets = types.ModuleType('PySide6.QtWidgets')
 qt_widgets.QMessageBox = object
+qt_widgets.QApplication = object
+qt_widgets.QDialog = object
+qt_widgets.QVBoxLayout = object
+qt_widgets.QTextEdit = object
+qt_widgets.QPushButton = object
 sys.modules.setdefault('PySide6.QtWidgets', qt_widgets)
 qt_core = types.ModuleType('PySide6.QtCore')
 qt_core.QDate = object
+qt_core.QRunnable = object
+qt_core.QObject = object
+
+class Signal:
+    def __init__(self, *args, **kwargs):
+        pass
+    def connect(self, *args, **kwargs):
+        pass
+    def emit(self, *args, **kwargs):
+        pass
+
+def Slot(*args, **kwargs):
+    def decorator(func):
+        return func
+    return decorator
+
+qt_core.Signal = Signal
+qt_core.Slot = Slot
+
+class _DummyPool:
+    @staticmethod
+    def start(*args, **kwargs):
+        pass
+
+class _DummyQThreadPool:
+    @staticmethod
+    def globalInstance():
+        return _DummyPool()
+
+qt_core.QThreadPool = _DummyQThreadPool
 sys.modules.setdefault('PySide6.QtCore', qt_core)
+
+# Stub easyocr so importing logic.ocr_paddle doesn't try to load heavy packages
+easyocr_stub = types.ModuleType('easyocr')
+easyocr_stub.Reader = object
+sys.modules.setdefault('easyocr', easyocr_stub)
 
 from logic.ocr_paddle import parse_fields, validate_with_rooms
 
